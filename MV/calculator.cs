3c2413dfcb75afa8f2f77c356f4ba0ae01d41e02
Me11ad <112916199+Me11ad@users.noreply.github.com>
2023-10-11 09:49:24 +0500

    Add files via upload

---
+++ MV/calculator.cs
@@ -0,0 +1,65 @@
+public class Calc{
+    public interface Sum(int x, int y)(
+        return x + y;
+    )
+}
+
+public class Requestor{
+    Calc calc;
+
+    public Requestor(Calc calc){
+        this.calc = calc;
+    }
+
+    public void Req(){
+        
+    }
+}
+
+public class Program{
+    static void Main(string | args){
+        Calc calc = new Calc();
+        Requestor req = new Requestor(calc);
+    }
+}
+
+
+char again = 'y';
+while(again == 'y');
+{
+    double a;
+    double b;
+    double result;
+    char operat;
+
+    Console.WriteLine("Введите первое число");
+    a = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Введите оператор");
+    operat = Convert.ToChar(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число");
+    b = Convert.ToDouble(Console.ReadLine());
+
+    if (operat == '+'){
+        result = a + b;
+        Console.WriteLine("Сумма = "+ result +);
+    }
+    else if (operat == '-'){
+        result = a - b;
+        Console.WriteLine("Разность = "+ result +);
+    }
+    else if(operat == '*'){
+        result = a * b;
+        Console.WriteLine("Умножение = "+ result +);
+    }
+    else if(operat == '/'){
+        result = a / b;
+        Console.WriteLine("Деление = "+ result +); 
+    }
+    else{
+        Console.WriteLine("Неизвестный оператор");
+    }
+    Console.WriteLine("Продолжить? (y/n)");
+    again = Convert.toChar(Console.ReadLine());
+}
