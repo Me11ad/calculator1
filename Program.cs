11e6c5cd1ab37c40b39dd1510c11ffe12f744b78
Me11ad <vlad505200483@gmail.com>
2023-11-14 11:26:03 +0500

    hren'

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 namespace calculator
 {
     class Program
-    {
+    {   
+        delegate void Operation1(int num1, int num2);
+        delegate void Operation2(int num1, int num2);
         static void Main(string[] args)
         {
             string value;
@@ -15,13 +19,23 @@ namespace calculator
                 int num2 = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Введите оператор: ");
                 string symbol = Console.ReadLine();
- 
-                switch (symbol)
-                {
-                    case "+":
+
+                Operation1 operation = Add;
+                int result = operation();
+                Console.WriteLine(result);
+
+                operation = Minus;
+                result = operation();
+                Console.WriteLine(result);
+
+                int Add(int num1, int num2) => num1 + num2;
+
+                int Minus(int num1, int num2) => num1 - num2;
+
+                class Add{
                         res = num1 + num2;
                         Console.WriteLine("Сложение: " + res);
-                        break;
+                    }   
                     case "-":
                         res = num1 - num2;
                         Console.WriteLine("Разность: " + res);
@@ -37,7 +51,7 @@ namespace calculator
                     default:
                         Console.WriteLine("Неверный оператор");
                         break;
-                }
+
                 Console.ReadLine();
                 Console.Write("Хотите продолжить?(д/н):");
                 value = Console.ReadLine();
